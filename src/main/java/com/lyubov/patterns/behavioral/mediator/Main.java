92fb4bde605c48ae2173e5fc8a62d3eb58a82737
Lubov <lubovruzanova@gmail.com>
2018-07-14 12:29:13 +0300

    add comments

--- src/main/java/com/lyubov/patterns/behavioral/mediator/Main.java
+++ src/main/java/com/lyubov/patterns/behavioral/mediator/Main.java
@@ -1,5 +1,19 @@
 package com.lyubov.patterns.behavioral.mediator;
 
+/**
+ * Предназначен для упрощения взаимодействия объектов системы путем создания
+ * специального объекта, который управляет распределением сообщений между остальными объектами.
+ *
+ * Реализация: чат
+ *
+ * LyubaChat - ConcreteMediator. Посредник, взаимодействующий с несколькими клиентскими классами. Основываясь на
+ * получаемой информации, посредник либо вызывает те или иные методы клиента, либо задействует некий общий метод,
+ * информирующий клиентов об изменении, либо выполняет обе операции.
+ *
+ * User и Admin - ConcreteClient. Обеспечивает реализацию каждого метода клиента. Класс ConcreteClient может среди
+ * прочих данных сохранять ссылку на экземпляр класса Mediator, чтобы через последний информировать других клиентов о
+ * тех или иных изменениях в своих данных.
+ */
 public class Main {
     public static void main(String[] args){
         LyubaChat lyubaChat = new LyubaChat();
@@ -9,11 +23,14 @@ public class Main {
         User vitaliy = new User(lyubaChat);
         vitaliy.setnickName("Vetch");
 
+        //добавляем пользователей в чат
         lyubaChat.setAdmin(lyubava);
         lyubaChat.addUser(vitaliy);
         lyubaChat.addUser(pashka);
 
         System.out.println("Чат Любы");
+
+        //тест чата
         vitaliy.sendMessage("Hi");
         lyubava.sendMessage("hi, gyus");
     }
--- src/main/java/com/lyubov/patterns/behavioral/template/Main.java
+++ src/main/java/com/lyubov/patterns/behavioral/template/Main.java
@@ -3,6 +3,15 @@ package com.lyubov.patterns.behavioral.template;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Предоставляет метод, который позволяет подклассам перекрывать части метода, не прибегая к их переписыванию.
+ *
+ * Реализация: Рецепты, где есть повторяющиеся действия - взять емкость, добавлять N-ое число ингредиентов, пр.
+ *
+ * CookingSoup - абстрактный класс / шаблон
+ * Borsh, Shchi - конкретные реализации
+ */
+
 public class Main {
     public static void main (String[] args){
         List<CookingSoups> soups = new ArrayList<>();
