f380e179eebbf0c42bdf8ec73d3bef419c58fbea
Lubov <lubovruzanova@gmail.com>
2018-07-23 12:10:27 +0300

    add builder

---
+++ src/main/java/creational/builder/Pizza.java
@@ -0,0 +1,56 @@
+package creational.builder;
+
+public class Pizza {
+    private boolean addCheese;
+    private boolean addPepperoni;
+    private boolean addHam;
+    private boolean addMushrooms;
+
+    public void getOrderPizza() {
+        System.out.println("Your pizza: ");
+        if (addMushrooms) {
+            System.out.println("with mushrooms");
+        }
+        if (addPepperoni) {
+            System.out.println("with pepperoni");
+        }
+        if (addHam) {
+            System.out.println("with ham");
+        }
+        if (addCheese) {
+            System.out.println("with cheese");
+        }
+    }
+
+    public boolean isAddCheese() {
+        return addCheese;
+    }
+
+    public void setAddCheese(boolean addCheese) {
+        this.addCheese = addCheese;
+    }
+
+    public boolean isAddPepperoni() {
+        return addPepperoni;
+    }
+
+    public void setAddPepperoni(boolean addPepperoni) {
+        this.addPepperoni = addPepperoni;
+    }
+
+    public boolean isAddHam() {
+        return addHam;
+    }
+
+    public void setAddHam(boolean addHam) {
+        this.addHam = addHam;
+    }
+
+    public boolean isAddMushrooms() {
+        return addMushrooms;
+    }
+
+    public void setAddMushrooms(boolean addMushrooms) {
+        this.addMushrooms = addMushrooms;
+    }
+}
---
+++ src/main/java/creational/builder/PizzaBuilder.java
@@ -0,0 +1,29 @@
+package creational.builder;
+
+public class PizzaBuilder {
+    Pizza pizza = new Pizza();
+
+    public PizzaBuilder addMushrooms() {
+        pizza.setAddMushrooms(true);
+        return this;
+    }
+
+    public PizzaBuilder addPepperoni() {
+        pizza.setAddPepperoni(true);
+        return this;
+    }
+
+    public PizzaBuilder addCheese() {
+        pizza.setAddCheese(true);
+        return this;
+    }
+
+    public PizzaBuilder addHam() {
+        pizza.setAddHam(true);
+        return this;
+    }
+
+    public Pizza build(){
+        return pizza;
+    }
+}
